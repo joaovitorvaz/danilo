292958d61f25616ba52b7a83a54c434b52f1f0a3
joaovitorvaz <70914862+joaovitorvaz@users.noreply.github.com>
2022-04-11 18:30:21 -0300

    a

--- consumer.py
+++ consumer.py
@@ -2,15 +2,7 @@ from basicClient import BasicPikaClient
 
 class BasicMessageReceiver(BasicPikaClient):
 
-    def get_message(self, queue):
-        method_frame, header_frame, body = self.channel.basic_get(queue)
-        if method_frame:
-            print(method_frame, header_frame, body)
-            self.channel.basic_ack(method_frame.delivery_tag)
-            return method_frame, header_frame, body
-        else:
-            print('No message returned')
-
+    
     def consume_messages(self, queue):
             def callback(ch, method, properties, body):
                 print(" [x] Received %r" % body)
@@ -44,10 +36,5 @@ if __name__ == "__main__":
     # Close connections.
     basic_message_receiver.close()
 
-    # Consume the message that was sent.
-    basic_message_receiver.get_message("hello world queue")
-
-    # Close connections.
-    basic_message_receiver.close()
 
     
