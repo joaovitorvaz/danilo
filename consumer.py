b1774742e9d7b95b4bbae01574ce332c9e5636d0
joaovitorvaz <70914862+joaovitorvaz@users.noreply.github.com>
2022-04-11 18:37:23 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -41,10 +41,7 @@ if __name__ == "__main__":
     # Consume multiple messages in an event loop.
     basic_message_receiver.consume_messages("hello world queue")
 
-   
 
-    # Consume the message that was sent.
-    basic_message_receiver.get_message("hello world queue")
 
     # Close connections.
     basic_message_receiver.close()
